79eb4c1fd7b120f32a9c08a3eadde1d72f4815e0
0verLighT <alexandre@0verlight.com>
2026-01-14 01:29:55 +0100

    chore: include philo

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,13 +6,18 @@
 /*   By: amartel <amartel@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 00:54:32 by amartel           #+#    #+#             */
-/*   Updated: 2026/01/10 01:49:51 by amartel          ###   ########.fr       */
+/*   Updated: 2026/01/14 01:21:16 by amartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-#include <stdio.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
 
 #endif
