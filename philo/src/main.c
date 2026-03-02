8e3a7da7b2fc3a3b54f89a621e569f5caadd5b4d
0verLighT <alexandre@0verlight.com>
2026-03-02 03:41:06 +0100

    build: update meson for pthread.h and makefile

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: amartel <amartel@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 00:55:11 by amartel           #+#    #+#             */
-/*   Updated: 2026/01/16 02:45:11 by amartel          ###   ########.fr       */
+/*   Updated: 2026/03/02 03:39:17 by amartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 
 static void info_prog(void)
 {
-	write(2, "./philo <number_of_philosophers> <time_to_die time_to_eat> ", 60);
-	write(2, "<time_to_sleep> [number_of_times_each_philo_must_eat]\n", 61);
+	(void) write(2, "./philo <number_of_philosophers> <time_to_die time_to_eat> ", 60);
+	(void) write(2, "<time_to_die time_to_eat> <time_to_sleep> ", 43);
+	(void) write(2, "[number_of_times_each_philo_must_eat]\n", 39);
 }
 
 void *print_message(void *arg)
